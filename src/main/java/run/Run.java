cf8c2f69f235cd45fa25b6585db03f87b9ff88c1
Yannis <grego.yannis@gmail.com>
2016-08-19 16:33:10 +0200

    Commit Run

--- src/main/java/run/Run.java
+++ src/main/java/run/Run.java
@@ -3,11 +3,23 @@ package run;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import dao.MediaDAO;
+import fr.dta.application.media.model.Media;
+import fr.dta.application.media.model.Type;
+
 public class Run {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("unit");
+//		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("unit");
+		MediaDAO mediaDAO = MediaDAO.instance();
+		
+		Media book1 = new Media("Don Quichotte", Type.Livre, "Miguel de Cervantes");
+		Media book2 = new Media("Fables", Type.Livre, "Esope");
+		
+		mediaDAO.addMedia(book1);
+		mediaDAO.addMedia(book2);
+		
 	}
 
 }
