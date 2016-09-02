5f25be74f473fe5a9fdf63e1ab87e030c5637cf5
germain <seulgege@hotmail.fr>
2016-09-02 14:01:58 +0200

    rien de spécial c'est pour faire un pull

--- app/js/biblio/biblio.js
+++ app/js/biblio/biblio.js
@@ -1,17 +1,7 @@
 angular.module('biblio', ['ngRoute'])
 	.config(function ($routeProvider) {
-		/*$routeProvider.when('/rencontre', {
-			templateUrl: 'template/jeu/rencontre.html',
-			controller: 'RencontreCtrl'
-		}).when('/bonjour/:name', {
-			templateUrl: 'template/jeu/bonjour.html',
-			controller: 'BonjourCtrl'
-		}).when('/boude', {
-			templateUrl: 'template/jeu/boude.html'
-		}).when('/jeu', {
-			templateUrl: 'template/jeu/jeu.html',
-			controller: 'JeuCtrl',
-		}).otherwise({
-			redirectTo:'/rencontre'
-		});*/
+		$routeProvider.when('/biblio', {
+			templateUrl: 'html/biblio/recherche-adherant.html',
+			controller: 'rechercheAdherantCtrl',
+		});
 	});
