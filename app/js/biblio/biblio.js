8b53f653ab7fbde7da1fd72eb34766a4b9a63760
Yannis <grego.yannis@gmail.com>
2016-09-02 10:48:49 +0200

    AngularJS integration

---
+++ app/js/biblio/biblio.js
@@ -0,0 +1,17 @@
+angular.module('biblio', ['ngRoute'])
+	.config(function ($routeProvider) {
+		/*$routeProvider.when('/rencontre', {
+			templateUrl: 'template/jeu/rencontre.html',
+			controller: 'RencontreCtrl'
+		}).when('/bonjour/:name', {
+			templateUrl: 'template/jeu/bonjour.html',
+			controller: 'BonjourCtrl'
+		}).when('/boude', {
+			templateUrl: 'template/jeu/boude.html'
+		}).when('/jeu', {
+			templateUrl: 'template/jeu/jeu.html',
+			controller: 'JeuCtrl',
+		}).otherwise({
+			redirectTo:'/rencontre'
+		});*/
+	});
