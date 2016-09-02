81059db63a53edeb6056b485509c0dd5f61a0690
germain <seulgege@hotmail.fr>
2016-09-02 17:42:53 +0200

    salut tout lemonde

--- app/js/biblio/biblio.js
+++ app/js/biblio/biblio.js
@@ -6,8 +6,8 @@ angular.module('biblio', ['ngRoute'])
 			controller: 'ModificationMediaCtrl'			
 		});
 		$routeProvider.when('/recherche-adherent', {
-			templateUrl: 'html/biblio/recherche-adherant.html',
-			controller: 'rechercheAdherantCtrl',		
+			templateUrl: 'html/biblio/recherche-adherent.html',
+			controller: 'rechercheAdherentCtrl',		
 		});		
 		$routeProvider.when('/modification-adherent', {
 			templateUrl: 'html/biblio/modification-adherent.html'
