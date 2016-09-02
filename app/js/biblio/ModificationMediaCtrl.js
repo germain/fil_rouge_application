205e12ccc5991c5c34e392112edb232983d09e5c
nathan levasseur <icietmaintenant62@gmail.com>
2016-09-02 13:15:06 +0200

    Maj Nathan

---
+++ app/js/biblio/ModificationMediaCtrl.js
@@ -0,0 +1,3 @@
+angular.module('biblio').controller('ModificationMediaCtrl', function($scope, $routeParams){
+			$scope.id = $routeParams.id;				
+});
--- app/js/biblio/biblio.js
+++ app/js/biblio/biblio.js
@@ -1,17 +1,10 @@
 angular.module('biblio', ['ngRoute'])
 	.config(function ($routeProvider) {
-		/*$routeProvider.when('/rencontre', {
-			templateUrl: 'template/jeu/rencontre.html',
-			controller: 'RencontreCtrl'
-		}).when('/bonjour/:name', {
-			templateUrl: 'template/jeu/bonjour.html',
-			controller: 'BonjourCtrl'
-		}).when('/boude', {
-			templateUrl: 'template/jeu/boude.html'
-		}).when('/jeu', {
-			templateUrl: 'template/jeu/jeu.html',
-			controller: 'JeuCtrl',
-		}).otherwise({
-			redirectTo:'/rencontre'
-		});*/
+		$routeProvider.when('/modification-media/:id', {
+			templateUrl: '/html/biblio/modification-media.html', 
+			controller: 'ModificationMediaCtrl'			
+		});
+		$routeProvider.otherwise({
+			redirectTo: '/404'			
+		});
 	});
