919d46d30ea11cb6d5b73c561d074e853e00d4a1
Yannis <grego.yannis@gmail.com>
2016-09-05 16:07:42 +0200

    Push of service concerning adherent modification

--- app/js/biblio/service/serviceAdherent.js
+++ app/js/biblio/service/serviceAdherent.js
@@ -1,49 +1,32 @@
 angular.module('biblio')
-	.factory('serviceAdherent', function ($http) {
+	.factory('serviceAdherent', function ($http, $q) {
 		var HOST = "http://192.168.10.41:8090/";
-		var PATH_NAME_SEARCH = "resource/adherent.recherche/";
-		var PATH_NAME_MODIFY = "resource/adherent.modification/";
+		var PATH_NAME_ACCESSION = "resource/adherent.accession";
+		var PATH_NAME_MODIFY = "resource/adherent.modification";
 		var METHOD_POST = "POST";
 
-		var searchObjectInArray = function(data, id) {
-			var res = null;
-			angular.forEach(data, function(element) {
-				if (element.id == id) {
-					res = element;
-				}
-			});
-
-			return res;
-		};
-
-		var concatParams = function(params) {
-			var concatParams = "?";
-			angular.forEach(params, function(value, key) {
-				concatParams += key + "=" + value + "&"
-			});
-			return concatParams;
-		};
-
 		return {
-			searchAdherent: function(params) {
-				var completeUrl = HOST + PATH_NAME_SEARCH + concatParams(params);
-				return $http.get(completeUrl).then(function(response) {
+			getAdherent: function(id) {
+				var completeUrl = HOST + PATH_NAME_ACCESSION;
+				return $http.get(completeUrl, {params : {"id":id}}).then(function(response) {
+					console.log(response);
 					return response.data;
+				}, function(error) {
+					return $q.reject(error);
 				});
 			},
 			modifyAdherent: function(adherent) {				
 				var req = {
 					method: METHOD_POST,
 					url: HOST + PATH_NAME_MODIFY,
-					/*headers: {
-					   'Content-Type': undefined
-					},*/
 					data: adherent 
 				};
 				
 				return $http(req).then(function (response) {				 
 					return response; 
-			  	});
+			  	}, function(error) {
+					return $q.reject(error);
+				});
 				
 			}
 		};
