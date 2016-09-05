d1fbc97498287e134eed472241e98063e5bf42ba
Yannis <grego.yannis@gmail.com>
2016-09-05 18:19:48 +0200

    -Addition of errors displaying on modification adherent view\n -Update of code getting an adherent into adherent modification controller\n -Managing of errors in service and controller adherent

--- app/js/biblio/controller/ModificationAdherentCtrl.js
+++ app/js/biblio/controller/ModificationAdherentCtrl.js
@@ -1,5 +1,5 @@
 angular.module('biblio')
-	.controller('ModificationAdherentCtrl', function($scope, $routeParams, serviceAdherent){
+	.controller('ModificationAdherentCtrl', function($scope, $routeParams, serviceAdherent, $timeout) {
 		var handleAdress = function(adress) {
 			var adressInfoName1 = "ligne1";
 			var adressInfoName2 = "ligne2";
@@ -21,19 +21,30 @@ angular.module('biblio')
 			return adressStr;
 		};
 
-		serviceAdherent.searchAdherent($routeParams).then(function(ad) {
-			var adherentFound = ad[0]; //In this case only one adherent
-			
-			var adherentTmp = angular.copy(adherentFound); 
-			adherentTmp.date_naissance = new Date(adherentFound.date_naissance);
-			adherentTmp.adresse = adherentFound.adresse;
+		serviceAdherent.getAdherent($routeParams.id).then(function(ad) {
+			var adherentTmp = angular.copy(ad); 
+			adherentTmp.date_naissance = new Date(ad.date_naissance);
+			adherentTmp.adresse = ad.adresse;
 
 			$scope.adherent = adherentTmp;
+		}, function(error) {
+			/*console.log(error.message);*/
+			$scope.error = error.data;
+			console.log(error.data.message);
 		});
 
 		$scope.modifyAdherent = function(adherent) {
 			serviceAdherent.modifyAdherent(adherent).then(function(response) {
-				console.log(response)
+				console.log(response);
+				$scope.modificationSuccess = true;
+				$scope.modifAdhForm.$setPristine(true);
+				$timeout(function() {
+					$scope.modificationSuccess = false;
+				}, 2000);
+			}, function(error) {
+				/*console.log(error.message);*/
+				console.log(error.data.message);
+				$scope.error = error.data;
 			});
 		};
 
