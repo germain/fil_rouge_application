7072757f08e6641121f14bd5e10ad16d767c4e21
jocelyn chene <jocelynchene@gmail.com>
2016-09-05 16:43:37 +0200

    les messages d'erreurs ne s'affichent plus que lorsque les champs ne sont pas bien renseignes
    la creation d'adherent ne se fait que lorsque les champs sont valides

--- app/js/biblio/controller/CreationAdherentCtrl.js
+++ app/js/biblio/controller/CreationAdherentCtrl.js
@@ -1,14 +1,30 @@
-angular.module('biblio').controller('CreationAdherentCtrl', function($scope, $routeParams, serviceAdherent){
+angular.module('biblio').controller('CreationAdherentCtrl',	function($scope, $routeParams, serviceAdherent) {
 			$scope.id = $routeParams.id;
-			
-			$scope.validAdherent = function(){
-				console.log("Controleur creation d'adherent "
-						+ $scope.toAddAdherent.nom + " " + $scope.toAddAdherent.prenom + " " + $scope.toAddAdherent.dateNat
-						+ " " + $scope.toAddAdherent.email+ " " + $scope.toAddAdherent.adresse+ " " + $scope.toAddAdherent.codePost
-						+ " " + $scope.toAddAdherent.ville+ " " + $scope.toAddAdherent.datePaie+ " " + $scope.toAddAdherent.montant);
-				
-				
-				serviceAdherent.createAdherent($scope.toAddAdherent.nom, $scope.toAddAdherent.prenom,$scope.toAddAdherent.dateNat, $scope.toAddAdherent.email,
-						$scope.toAddAdherent.adresse, $scope.toAddAdherent.codePost, $scope.toAddAdherent.ville, $scope.toAddAdherent.datePaie, $scope.toAddAdherent.montant);
-			};
-});
+
+			$scope.validAdherent = function() {
+				if ($scope.formAdh.$valid) {
+					console.log("Controleur creation d'adherent "
+							+ $scope.toAddAdherent.nom + " "
+							+ $scope.toAddAdherent.prenom + " "
+							+ $scope.toAddAdherent.dateNat + " "
+							+ $scope.toAddAdherent.email + " "
+							+ $scope.toAddAdherent.adresse + " "
+							+ $scope.toAddAdherent.codePost + " "
+							+ $scope.toAddAdherent.ville + " "
+							+ $scope.toAddAdherent.datePaie + " "
+							+ $scope.toAddAdherent.montant);
+
+					serviceAdherent.createAdherent($scope.toAddAdherent.nom,
+							$scope.toAddAdherent.prenom,
+							$scope.toAddAdherent.dateNat,
+							$scope.toAddAdherent.email,
+							$scope.toAddAdherent.adresse,
+							$scope.toAddAdherent.codePost,
+							$scope.toAddAdherent.ville,
+							$scope.toAddAdherent.datePaie,
+							$scope.toAddAdherent.montant);
+				}
+				;
+			}
+
+		});
