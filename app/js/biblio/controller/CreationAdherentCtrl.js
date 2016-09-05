494495f82403803ed07e6874bb89b1d73643c70e
jocelyn chene <jocelynchene@gmail.com>
2016-09-05 11:51:00 +0200

    commentaire media
    début de la fonction d'ajout d'adherent

--- app/js/biblio/controller/CreationAdherentCtrl.js
+++ app/js/biblio/controller/CreationAdherentCtrl.js
@@ -1,5 +1,7 @@
 angular.module('biblio').controller('CreationAdherentCtrl', function($scope, $routeParams){
 			$scope.id = $routeParams.id;
 			
-			$scope.validAdherent = function()
+			$scope.validAdherent = function(){
+				console.log("Controleur creation d'adherent " + $scope.toAddMedia.titre + " " + $scope.toAddMedia.auteur + " " + $scope.toAddMedia.type);
+			};
 });
--- app/js/biblio/controller/creationMediaCtrl.js
+++ app/js/biblio/controller/creationMediaCtrl.js
@@ -1,11 +1,11 @@
 angular.module('biblio')
 	.controller('CreationMediaCtrl', function($scope, $location, $routeParams, media){
 
-		$scope.ajoutMedia = function(){
-			if($scope.formCreationMedia.$valid){
-				//$location.url('/recherche-media');
-			}
-		}
+//		$scope.ajoutMedia = function(){
+//			if($scope.formCreationMedia.$valid){
+//				//$location.url('/recherche-media');
+//			}
+//		}
 				
 		$scope.ajoutmedia = function(){
 			console.log("Controleur creation du media " + $scope.toAddMedia.titre + " " + $scope.toAddMedia.auteur + " " + $scope.toAddMedia.type);
