80ec53820314105b2f1b0c6690caaf3b51e12948
nathan levasseur <icietmaintenant62@gmail.com>
2016-09-06 09:55:27 +0200

    MAJ biblio

--- app/js/biblio/biblio.js
+++ app/js/biblio/biblio.js
@@ -1,12 +1,11 @@
 angular.module('biblio', ['ngRoute', 'ngMessages', 'ui.bootstrap'])
-	.config(function ($routeProvider) {
-		
+	.config(function ($routeProvider) {		
 		$routeProvider.when('/creation-media', {
-			templateUrl: 'view/biblio/creation-media.html',
+			templateUrl: '/view/biblio/creation-media.html',
 			controller: 'CreationMediaCtrl'
 		});
 		$routeProvider.when('/connexion', {
-			templateUrl: 'view/biblio/connexion.html',
+			templateUrl: '/view/biblio/connexion.html',
 			controller: 'ConnexionCtrl'
 				         
 		});
@@ -14,34 +13,27 @@ angular.module('biblio', ['ngRoute', 'ngMessages', 'ui.bootstrap'])
 			templateUrl: '/view/biblio/modification-media.html', 
 			controller: 'ModificationMediaCtrl'			
 		});
-
 		$routeProvider.when('/recherche-adherent', {
 			templateUrl: 'view/biblio/recherche-adherent.html',
-			controller: 'rechercheAdherentCtrl'		
-			templateUrl: 'html/biblio/recherche-adherent.html',
-			controller: 'rechercheAdherentCtrl'	
+			controller: 'rechercheAdherentCtrl',		
+			templateUrl: 'html/biblio/recherche-adherent.html'		
 		});	
-
 		$routeProvider.when('/modification-adherent/:id', {
 			templateUrl: 'view/biblio/modification-adherent.html',
 			controller: 'ModificationAdherentCtrl'
-		});
-	
+		});	
 		$routeProvider.when('/recherche-media', {
 			templateUrl: '/view/biblio/recherche-media.html',
 			controller: 'RechercheMediaCtrl'	
-		});
-		
+		});		
 		$routeProvider.when('/recherche-media/:titre', {
 			templateUrl: '/view/biblio/recherche-media.html',
 			controller: 'RechercheMediaCtrl'	
 		});
-
 		$routeProvider.when('/creation-adherent', {
 			templateUrl: '/view/biblio/creation-adherent.html',
 			controller: 'CreationAdherentCtrl'
 		});
-
 		$routeProvider.otherwise({
 			redirectTo:'/connexion'
 		});
