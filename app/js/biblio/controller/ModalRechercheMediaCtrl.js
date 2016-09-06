b05368b2fa7d6f86458d6e810ab99e24fc8c7d70
nathan levasseur <icietmaintenant62@gmail.com>
2016-09-06 11:20:04 +0200

    recherche media modal

---
+++ app/js/biblio/controller/ModalRechercheMediaCtrl.js
@@ -0,0 +1,9 @@
+angular.module('biblio').controller('ModalRechercheMediaCtrl', function($scope, $routeParams, media, param){
+			
+			console.log("dd", param);
+			console.log("Controleur du modal ");		
+			$scope.modificationmedia = function(){
+				console.log("Controleur du modal ");				
+			};
+			
+});
