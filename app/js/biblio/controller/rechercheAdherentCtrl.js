e602f184e68838d817f54e6d7c4420593655195b
nathan levasseur <icietmaintenant62@gmail.com>
2016-09-06 17:20:12 +0200

    Accordéon recherche adhérent

--- app/js/biblio/controller/rechercheAdherentCtrl.js
+++ app/js/biblio/controller/rechercheAdherentCtrl.js
@@ -1,28 +1,29 @@
 angular.module("biblio").controller('rechercheAdherentCtrl',
 		function($scope, $location, $routeParams, $filter, $uibModal, serviceAdherent) {
-			$scope.RechercheAhderent = function() {
-				console.log("Controleur recherche adherent " + $scope.Adherent.id + " nom: " + $scope.Adherent.nom);
+	
+		$scope.RechercheAhderent = function() {
+			console.log("Controleur recherche adherent " + $scope.Adherent.id + " nom: " + $scope.Adherent.nom);
 
-				if ($scope.Adherent.id != null) {
-					var param = {
-						"id" : $scope.Adherent.id
-					};
-				} else if ($scope.Adherent.nom != null) {
-					var param = {
-						"nom" : $scope.Adherent.nom
-					};
-				} else {
-					var param = {
-						"id" : $scope.Adherent.id,
-						"nom" : $scope.Adherent.nom
-					};
-				}
-				
-				serviceAdherent.searchAdherent(param).then(function(data) {
-					$scope.list = data;
-					console.log(data);
-				}
+			if ($scope.Adherent.id != null) {
+				var param = {
+					"id" : $scope.Adherent.id
+				};
+			} else if ($scope.Adherent.nom != null) {
+				var param = {
+					"nom" : $scope.Adherent.nom
+				};
+			} else {
+				var param = {
+					"id" : $scope.Adherent.id,
+					"nom" : $scope.Adherent.nom
+				};
+			}			
+			serviceAdherent.searchAdherent(param).then(function(data) {
+				$scope.list = data;
+				console.log(data);
+			}
 		 )};
+		 
          $scope.tri = function(colonne){
                  $scope.maVarDeTrie = colonne;
                  /*console.log("variable tri" + $scope.maVarDeTrie);*/
@@ -31,16 +32,28 @@ angular.module("biblio").controller('rechercheAdherentCtrl',
 			$scope.orderByName = function() {
 			};
 
-			$scope.open = function() {
+//			$scope.open = function() {
+//				$uibModal.open({
+//					templateUrl : 'view/modal/recherche-adherent.html',
+//					controller : 'ModalRechercheAdherentCtrl',
+//					resolve : {
+//						param : {
+//							id : '5'
+//						}
+//					}
+//				});
+//			}
+			
+			$scope.open = function () {		
 				$uibModal.open({
-					templateUrl : 'view/modal/recherche-adherent.html',
-					controller : 'ModalRechercheAdherentCtrl',
-					resolve : {
-						param : {
-							id : '5'
-						}
-					}
-				});
-			}
+				      templateUrl: 'view/modal/recherche-adherent.html',
+				      controller: 'ModalRechercheAdherentCtrl',
+				      resolve: {
+					        param: {
+					        	id : '5'
+					        }
+					      }
+				    });
+			 };
 		});
        
