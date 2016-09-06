85ac385f072a6940b895627431272f46c52d1bd5
nathan levasseur <icietmaintenant62@gmail.com>
2016-09-06 12:25:05 +0200

    Modal media mise en forme

--- app/js/biblio/controller/ModalRechercheMediaCtrl.js
+++ app/js/biblio/controller/ModalRechercheMediaCtrl.js
@@ -1,4 +1,4 @@
-angular.module('biblio').controller('ModalRechercheMediaCtrl', function($scope, $uibModal, media, param, $uibModalInstance){
+angular.module('biblio').controller('ModalRechercheMediaCtrl', function($scope, $uibModal, media, param, $uibModalInstance, serviceEmprunt){
 			
 			console.log("dd", param.id);
 			
@@ -18,5 +18,15 @@ angular.module('biblio').controller('ModalRechercheMediaCtrl', function($scope, 
 				console.log('fds');
 				$uibModalInstance.close();
 			};
+			
+//			serviceEmprunt.postEmprunt(nom, date_retour, date_emprunt).then(function(data){				
+//				console.log(data);
+//				$scope.toAddMedia = {
+//									id :data.id,
+//									titre :data.titre,
+//									auteur :data.auteur, 
+//									type:data.type
+//									};
+//			});	
 			
 });
--- app/js/biblio/service/serviceEmprunt.js
+++ app/js/biblio/service/serviceEmprunt.js
@@ -1,4 +1,16 @@
-angular.module('biblio')
-	.factory('serviceEmprunt', function ($http, $q) {
-		
-	});
+angular.module('biblio').factory('serviceEmprunt', function ($http, $q) {
+	return {		
+			postEmprunt : function(id_adh, date_retour, date_emprunt){				
+			//console.log("Webservice modification du media - " + id + " - "+ titre + " - " + auteur + " - " + type);
+			$http.post('http://192.168.10.41:8090/resource/emprunt.ajout', { id: id, 
+			 	 id_adherent : id_adh,
+			 	 id_media : auteur,
+			 	 depart : type
+		 	   }).then(function (response) {				 
+				  //return response.data; 
+				  console.log(response);
+			  });
+			
+		}
+	}
+});
