59748c4e6505320e334159a3ec6bb6ad1e1072ef
nathan levasseur <icietmaintenant62@gmail.com>
2016-09-06 15:02:32 +0200

    MAJ

--- app/js/biblio/service/serviceEmprunt.js
+++ app/js/biblio/service/serviceEmprunt.js
@@ -1,8 +1,9 @@
 angular.module('biblio').factory('serviceEmprunt', function ($http, $q, $filter) {
 	return {		
-			postEmprunt : function(id_adherent, id_media, depart){				
-				var myDate = $filter('date')(depart, "yyyy-MM-dd");
-				console.log("Webservice serviceEmprunt - " + id_adherent + " - "+ id_media + " - " + myDate + " - ");
+			postEmprunt : function(id_adherent, id_media, depart){
+				var myDate = new Date(depart);
+				myDate = $filter('date')(depart, "yyyy-MM-dd");
+				//console.log("Webservice serviceEmprunt - " + id_adherent + " - "+ id_media + " - " + myDate + " - ");
 				$http.post('http://192.168.10.41:8090/resource/emprunt.ajout', { 
 				 	 id_adherent : id_adherent,
 				 	 id_media : id_media,
