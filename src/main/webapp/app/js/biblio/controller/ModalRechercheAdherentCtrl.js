b134b32e14f88b230b874fe785ec6a6d2c4a165f
jocelyn chene <jocelynchene@gmail.com>
2016-09-26 17:21:44 +0200

    recherche d'adherent adaptée, adaptation des emprunts

--- src/main/webapp/app/js/biblio/controller/ModalRechercheAdherentCtrl.js
+++ src/main/webapp/app/js/biblio/controller/ModalRechercheAdherentCtrl.js
@@ -1,9 +1,17 @@
 angular.module('biblio')
-.controller('ModalRechercheAdherentCtrl', function($scope, $uibModal, serviceAdherent, param, $uibModalInstance){
+.controller('ModalRechercheAdherentCtrl', function($scope, $uibModal, serviceAdherent, param, $uibModalInstance, serviceEmprunt){
 	console.log("ModalRechercheAdherentCtrl");
+	
+	$scope.emprunt = {};
+	
+	$scope.emprunt.id_emprunteur = param.id;
 	
 	serviceAdherent.getAdherent(param.id).then(function(data){				
 		console.log(data);
 		$scope.adherentDetails = angular.copy(data);
 	});	
+	
+	$scope.addEmprunt = function() {
+		serviceEmprunt.postEmprunt($scope.emprunt.id_emprunteur, $scope.emprunt.id_media, $scope.emprunt.date_emprunt);
+	}
 });
--- src/main/webapp/app/js/biblio/service/serviceEmprunt.js
+++ src/main/webapp/app/js/biblio/service/serviceEmprunt.js
@@ -1,16 +1,17 @@
 angular.module('biblio').factory('serviceEmprunt', function ($http, $q, $filter) {
+	var serveur = 'localhost';
+	var port = '8080';
+	var application = 'bib/rest';
 	return {		
 			postEmprunt : function(id_adherent, id_media, depart){
 				var myDate = new Date(depart);
 				myDate = $filter('date')(depart, "yyyy-MM-dd");
-				//console.log("Webservice serviceEmprunt - " + id_adherent + " - "+ id_media + " - " + myDate + " - ");
-				$http.post('http://localhost:8090/resource/emprunt.ajout', { 
+				$http.post('http://'+serveur+':'+port+'/'+application+'/emprunt', { 
 				 	 id_adherent : id_adherent,
 				 	 id_media : id_media,
 				 	 depart : myDate
 			 	   }).then(function (response) {				 
 					  return response.data; 
-					  //console.log(response);
 				  });				
 			}
 	}
