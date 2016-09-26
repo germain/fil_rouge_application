277c67148160213e58e0e93422d13ffeb8965409
hela soussi <hela.soussii@gmail.com>
2016-09-26 09:58:51 +0200

    Service recherche Media par title, author et type. Recherche par type n'est pas bien finie

--- src/main/java/fr/dta/mediatheque/application/service/impl/MediaServiceImpl.java
+++ src/main/java/fr/dta/mediatheque/application/service/impl/MediaServiceImpl.java
@@ -1,5 +1,6 @@
 package fr.dta.mediatheque.application.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -17,6 +18,7 @@ import org.springframework.stereotype.Service;
 
 import fr.dta.mediatheque.application.core.JpaRepository;
 import fr.dta.mediatheque.application.model.Media;
+import fr.dta.mediatheque.application.model.Type;
 import fr.dta.mediatheque.application.repository.MediaRepository;
 import fr.dta.mediatheque.application.service.MediaService;
 @Service ("mediaService")
@@ -32,16 +34,40 @@ public class MediaServiceImpl implements MediaService {
 			return repository.findAllMedia();
 		}
 
+
+//		@Override
+//		public List<Media> findMedia(Media media) {
+//			CriteriaQuery<Media> criteria = repository.newCriteria();
+//			CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+//			Root<Media> from = criteria.from(Media.class);
+//			
+//			return null;
+//		}
+
 		@Override
 		public List<Media> findMedia(Media media) {
 			CriteriaQuery<Media> crit = repository.newCriteria();
 			CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
 			Root<Media> from = crit.from(Media.class);
+			List<Predicate> predicates = new ArrayList<Predicate>();
 			//ajouter les conditions sur les predicats
-			Predicate predicate1 = criteriaBuilder.equal(from.get("title"), media.getTitle());
-			Predicate predicate2 = criteriaBuilder.equal(from.get("type"), media.getType());
-			Predicate predicate3 = criteriaBuilder.equal(from.get("author"), media.getAuthor());
-			crit.where(criteriaBuilder.and(predicate1,predicate2,predicate3));
+			if(media.getTitle() != null){
+			Predicate predicate1 = criteriaBuilder.like(criteriaBuilder.upper(from.get("title")), "%"+media.getTitle().toUpperCase().trim()+"%");
+			predicates.add(predicate1);
+			}
+			if(media.getType() != null){
+			Predicate predicate2 = criteriaBuilder.equal((from.get("type")),  media.getType());
+			System.out.println(media.getType().toString().toUpperCase());
+			predicates.add(predicate2);
+			}
+			if(media.getAuthor() != null){
+			Predicate predicate3 = criteriaBuilder.like(criteriaBuilder.upper(from.get("author")),"%"+ media.getAuthor().toUpperCase().trim()+"%");
+			predicates.add(predicate3);
+			}
+			 for (Predicate predicate : predicates) {
+		            crit.where(criteriaBuilder.or(predicate), criteriaBuilder.and(predicate));
+		        }
 			return em.createQuery(crit).getResultList();
 		}
+		
 }
